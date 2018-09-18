a931a2152c04adf3840c8849e92ca55ca2a4d11d
LOubouzar <LOubouzar@gmail.com>
2018-09-17 21:45:16 -0400

    Added JS and server logic

---
+++ app/routing/apiroutes.js
@@ -0,0 +1,40 @@
+//Load Data from friends.js
+var friends = require("../data/friends");
+
+module.exports = function(app) {
+
+    app.get("/api/friends", function (req, res){
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function(req, res) {
+                
+        var userInput = req.body;
+               
+        for (var i = 0; i < userInput.scores.length; i++) {
+            userInput.scores[i] = parseInt(userInput.scores[i]);
+        }
+
+        var matchIndex =0;
+        var matchDifference = 500;
+
+        for(var i =0; i< friends.length; i++) {
+            var totalDifference =0;
+            for (var j =0; j < friends[i].scores.length; j++) {
+                var difference = math.abs(user.scores[j] - friends[i].scores[j]);
+                totalDifference += difference;
+            }
+            if (totalDifference < matchDifference) {
+                matchIndex = i;
+                matchDifference = totalDifference;
+            } 
+        };
+    
+        friends.push(userInput);
+
+        res.json(friends[matchIndex]);
+    
+    });
+
+
+};
--- server.js
+++ server.js
@@ -3,4 +3,14 @@ var bodyparser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+require("./app/routing/apiroutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
